2bf26017e09f9410561e185f9b03fb7153120a92
Loris <loris4526@gmail.com>
2019-03-07 11:53:33 +0100

    proper implementation for getcommand(); readln()

--- src/readln.c
+++ src/readln.c
@@ -3,6 +3,34 @@
 #include <string.h>
 
 char getcommand() {
-    printf("getcommmand\n");
-    return 'q';
+    char command[MAXSTRLEN];
+    int commandlen;
+    char c;
+
+    commandlen = readln(command);
+    if (commandlen != 1) {
+        c = 'x';
+    } else {
+        c = command[0];
+    }
+    return c;
+}
+
+int readln(char s[]) {
+    char ch;
+    int i;
+    int chars_remain;
+    i = 0;
+    chars_remain = 1;
+    while (chars_remain) {
+        ch = getchar();
+        if ((ch == '\n') || (ch == EOF)) {
+            chars_remain = 0;
+        } else if (i < MAXSTRLEN - 1) {
+            s[i] = ch;
+            i++;
+        }
+    }
+    s[i] = '\0';
+    return i;
 }
